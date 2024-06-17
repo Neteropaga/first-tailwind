51adc24c207143ba206a86c714e48d52cf2f3512
John <johnitehero@gmail.com>
2024-06-18 00:35:10 +0200

    add files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      couleur1: "#00040f",
+      couleur2: "#00f6ff",
+      couleur3: "rgba(255,255,255,0.7)",
+      couleur4: "rgba(9,151,124,0.1)",
+      couleur5: "rgb(255,255,255)",
+      couleur6: "rgb(0,0,0)",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
